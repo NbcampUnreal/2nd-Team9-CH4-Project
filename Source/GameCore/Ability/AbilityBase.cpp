879bd220cc5d0465a42fcdc989d940444b20efba
Jihyeon0 <wjswlqhd1@naver.com>
2025-04-04 16:12:30 +0900

    Revert "Commit: 어빌리티 매니저, 어빌리티베이스 틀 추가"
    This reverts commit de9f7a9561751324496dd4071abad6487debe787.

--- Source/GameCore/Ability/AbilityBase.cpp
+++ Source/GameCore/Ability/AbilityBase.cpp
@@ -1,47 +1,26 @@
-﻿#include "AbilityBase.h"
-#include "GameFramework/Character.h"
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
-UAbilityBase::UAbilityBase()
+#include "AbilityBase.h"
+
+
+// Sets default values
+AAbilityBase::AAbilityBase()
 {
-	//PrimaryActorTick.bCanEverTick = true;
-	OwnerCharacter = nullptr; //임시
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 }
 
-void UAbilityBase::Initialize(ACharacter* InOwner)
+// Called when the game starts or when spawned
+void AAbilityBase::BeginPlay()
 {
-	OwnerCharacter = InOwner;
-	bIsActive = false;
+	Super::BeginPlay();
+	
 }
 
-bool UAbilityBase::CanActivate()
+// Called every frame
+void AAbilityBase::Tick(float DeltaTime)
 {
-	//부모에서 1차 검사(스턴, 죽음 등등) 또는 자식에서 부분만족 등 조건 변경, Changes to the first test (stun, death, etc.) in parents or partial satisfaction in children
-	if (!OwnerCharacter)
-	{
-		return false;
-	}
-	//const FGameplayTagContainer& PlayerTagContainer = OwnerCharacter->GetCurrentTags(); // 혹은 상태별로 가지는 태그, 플레이어에서 함수 만들기, create a function in a tag, a player
-	FGameplayTagContainer PlayerTagContainer;
-	return PlayerTagContainer.HasAny(UnqiueRequiredTags); //어빌리티가 필요한 태그를 가지고 있는지, 필요에 따라서 자식에서 부분만족 등 조건 변경, Whether you have a tag that requires Ability, or changes the conditions for partial satisfaction in your child as needed
+	Super::Tick(DeltaTime);
 }
 
-void UAbilityBase::Activate()
-{
-	/*
-	if(CanActivate()) 태그 체크, Tag check
-	{
-		PlayMontage(); 몽타주에서 특정 프레임에 이펙트 생성 등등 이벤트
-	}
-	*/
-}
-
-void UAbilityBase::PlayMontage()
-{
-	OwnerCharacter->PlayAnimMontage(AbilityMontage);
-}
-
-void UAbilityBase::OnAbilityFinished()
-{
-	bIsActive = false;
-}
--- Source/GameCore/GameCore.Build.cs
+++ Source/GameCore/GameCore.Build.cs
@@ -10,7 +10,6 @@ public class GameCore : ModuleRules
             new string[]
             {
                 "Core",
-                "GameplayTags"
             }
         );
 
