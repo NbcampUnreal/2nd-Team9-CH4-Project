338d921b943a0c53aec5884197b8e27e0eb3a6fd
Cat Yubi <kimtaewan144@gmail.com>
2025-04-09 22:11:31 +0900

    Update

---
+++ Source/GameCore/Fighter/Test/TestPlayer.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "TestPlayer.generated.h"
+
+UCLASS()
+class GAMECORE_API ATestPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ATestPlayer();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Montage")
+	TArray<UAnimMontage*> AbilityMontage;
+};
