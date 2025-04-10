a6fe0f96af340ce1ac6654ac7f720fa1f70a6649
bakkyutae <rbxo3116@gmail.com>
2025-04-10 21:16:52 +0900

    add UI

--- Source/GameUI/UI/ModeWidget.h
+++ Source/GameUI/UI/ModeWidget.h
@@ -17,38 +17,80 @@ UCLASS()
 class GAMEUI_API UModeWidget : public UUserWidget
 {
 	GENERATED_BODY()
+		
 
-public:
-	FGameplayTag UIDataRowTag;
-	
 protected:
-
-	virtual void NativeConstruct() override;
-
-	UFUNCTION()
-	void ButtonClickRight();
-
-	UFUNCTION()
-	void ButtonClickLeft();
-
-	UFUNCTION()
-	void UpdateText();
-
 	
-
+	virtual void NativeConstruct() override;
+	
 
 private:
+
+	void InitializeGameModes();
+	void UpdateDisplayedMode();
+
+	
+
+public:
+
+	//Mode
+
+	UFUNCTION(BlueprintCallable, Category="ModeSelect")
+	FGameplayTag ModesSelectTag()const;
+
+	UFUNCTION()
+	void ShiftModeRight();
+
+	UFUNCTION()
+	void ShiftModeLeft();
+
+	//Time
+
+	UFUNCTION(BlueprintCallable, Category = "ModeSelect")
+	FGameplayTag TimesSelectTag()const;
+
+	UFUNCTION(BlueprintCallable, Category = "ModeSelect")
+	void ShiftTimeRight();
+
+	UFUNCTION(BlueprintCallable, Category = "ModeSelect")
+	void ShiftTimeLeft();
+	
+
+private:
+
+	//Mode
 
 	UPROPERTY(meta = (BindWidget))
 	class UTextBlock* ModeNameText;
 	
 	UPROPERTY(meta = (BindWidget))
-	class UButton* RightButton;
+	class UButton* ModeRightButton;
 
 	UPROPERTY(meta = (BindWidget))
-	class UButton* LeftButton;
+	class UButton* ModeLeftButton;
+
+	UPROPERTY(EditAnywhere, Category ="ModeSelect")
+	TArray<FGameplayTag> ModeTags;
+
+	int32 CurrentIndex = 1;
 
 	
-	int32 CurrentIndex = 0;
+	// Time
 
-};
+	
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* TimeText;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* TimeRightButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* TimeLeftButton;
+
+	UPROPERTY(EditAnywhere, Category = "ModeSelect")
+	TArray<FGameplayTag> TimeTags;
+
+	int32 TimeIndex = 0;
+
+}; 
